114fb5508fac95bb202a562855970714cdfb9ecc
Ben Wakefield <wenbakefield@gmail.com>
2023-04-21 00:47:37 -0400

    trill square i2c integration with tremolo

--- firmware/axetron.ino
+++ firmware/axetron.ino
@@ -1,19 +1,23 @@
+
 // Title: AxeTron
 // Description: Onboard multieffects for electric guitar
 // Hardware: Daisy Seed
 // Author: Ben Wakefield
 
 #include "DaisyDuino.h"
+#include <Trill.h>
+#include <Wire.h>
 
 DaisyHardware hw;
+Trill trill;
 
-Limiter limiter;
 Flanger flanger;
-Phaser phaser;
 Tremolo tremolo;
 Autowah autowah;
 Compressor compressor;
-ReverbSc reverb;
+//ReverbSc reverb;
+Decimator decimator;
+//Looper looper;
 
 float knob1;
 float knob2;
@@ -22,13 +26,11 @@ float knob4;
 float knob5;
 float knob6;
 
-Switch button1;
-Switch button2;
-Switch button3;
+float trillX;
+float trillY;
 
-AnalogBassDrum bassdrum;
-AnalogSnareDrum snaredrum;
-HiHat<> hihat;
+Switch button1trig;
+int button1 = -1;
 
 void AudioCallback(float **in, float **out, size_t size) {
   for (size_t i = 0; i < size; i++) {
@@ -41,38 +43,36 @@ void AudioCallback(float **in, float **out, size_t size) {
       flanger.SetLfoDepth(knob2);
       output_sample = flanger.Process(output_sample);
     }
-    if (knob3 > 0.01) {
-      phaser.SetLfoDepth(knob3);
-      output_sample = phaser.Process(output_sample);
-    }
+    // if (knob3 > 0.01) {
+    //   phaser.SetLfoDepth(knob3);
+    //   output_sample = phaser.Process(output_sample);
+    // }
+
     if (knob4 > 0.01) {
       compressor.SetThreshold(knob4 * -80.0);
       output_sample = compressor.Process(output_sample);
     }
-    if (knob5 > 0.01) {
-      tremolo.SetDepth(knob5);
+
+    if (trillX > 0.06 || trillY > 0.06) {
+      tremolo.SetFreq(trillX * 10.0);
+      tremolo.SetDepth(trillY);
       output_sample = tremolo.Process(output_sample);
+    }
+
+    if (knob5 > 0.01) {
+      decimator.SetBitcrushFactor(knob5);
+      output_sample = decimator.Process(output_sample);
     }
     if (knob6 > 0.01) {
       autowah.SetDryWet(knob6 * 100);
       output_sample = autowah.Process(output_sample);
     }
 
-    // if (button1.RisingEdge()) {
-    //   output_sample = output_sample + hihat.Process(true);
-    // }
-    // if (button2.RisingEdge()) {
-    //   output_sample = output_sample + bassdrum.Process(true);
-    // }
-    // if (button3.RisingEdge()) {
-    //   output_sample = output_sample + snaredrum.Process(true);
-    // }
+    //output_sample = output_sample * (knob3 / 2.0);
+
 
     out[0][i] = out[1][i] = output_sample;
   }
-
-  //limiter.ProcessBlock(out[0], size, 0.0);
-  //limiter.ProcessBlock(out[1], size, 0.0);
 }
 
 void setup() {
@@ -80,9 +80,7 @@ void setup() {
   hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
   sample_rate = DAISY.get_samplerate();
 
-  limiter.Init();
   flanger.Init(sample_rate);
-  phaser.Init(sample_rate);
 
   tremolo.Init(sample_rate);
   tremolo.SetFreq(7.21);
@@ -92,15 +90,16 @@ void setup() {
   autowah.SetLevel(1.0);
   autowah.SetWah(0.5);
 
+  decimator.Init();
+
   compressor.Init(sample_rate);
 
-  //hihat.Init(sample_rate);
-  //bassdrum.Init(sample_rate);
-  //snaredrum.Init(sample_rate);  
+  button1trig.Init(1000, false, D0, INPUT);
 
-  button1.Init(1000, true, D0, INPUT);
-  button2.Init(1000, true, D1, INPUT);
-  button3.Init(1000, true, D2, INPUT);
+  Wire.begin();
+  trill.setup(Trill::TRILL_SQUARE, 0x28);
+
+  //pinMode(A6, OUTPUT);
 
   DAISY.begin(AudioCallback);
 }
@@ -113,9 +112,22 @@ void loop() {
   knob2 = analogRead(A1) / 1023.f;
   knob1 = analogRead(A0) / 1023.f;
 
-  button1.Debounce();
-  button2.Debounce();
-  button3.Debounce();
+  button1trig.Debounce();
+
+  trill.read();
+  float positionX = trill.touchHorizontalLocation(0);
+  float positionY = trill.touchLocation(0);
+  if (positionX >= 0) {
+    trillX = 1.0 - (positionX / 1800.0);
+  }
+  if (positionY >= 0) {
+    trillY = 1.0 - (positionY / 1800.0);
+  }
+
+  if (button1trig.RisingEdge()) {
+    button1 = button1 * -1;
+  }
+
 }
 
 float fuzz(float input, float level) {
